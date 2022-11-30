1c24955f1d08868f562cef468f925692133d3654
Jenny Lewis-Gandy <jenny.lewis-gandy@voxpopme.com>
2022-11-30 14:13:49 +0000

    form created in ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // variables
+  newForm: FormGroup;
+  projectStatus: ["Stable", "Critical", "Finished"]
+
+  // lifecycle hooks
+  ngOnInit() {
+    this.setUpNewForm();
+  };
+
+  // functions
+  setUpNewForm() {
+    this.newForm = new FormGroup({
+      'projectName': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl(this.projectStatus[0])
+    })
+  }
 }
